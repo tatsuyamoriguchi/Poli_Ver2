9a6f9b55fbdb10ccb62210b9c52e6f62e58d8b6b
Brian Moriguchi <brian@beckos.com>
2020-06-22 12:38:18 -0700

    Added comment to ShareViewController to disable Share Extension on MacVersion Disabled Share Extension for MacOS

--- Poli-Extension/ShareViewController.swift
+++ Poli-Extension/ShareViewController.swift
@@ -11,29 +11,30 @@ import Social
 import CoreData
 import MobileCoreServices
 
-
 class ShareViewController: SLComposeServiceViewController {
-    
-    
+//    class ShareViewController: UIViewController {
+  
     var selectedGoal: Goal?
     var goals = [Goal]()
     
     override func isContentValid() -> Bool {
-        
+
         // Do validation of contentText and/or NSExtensionContext attachments here
         if selectedGoal == nil || contentText.isEmpty {
+
             return false
+
         } else {
             return true
         }
-        
+
     }
     
     override func didSelectPost() {
-    
+
         let entity = NSEntityDescription.entity(forEntityName: "Task", in: context)
         let newBookmark = NSManagedObject(entity: entity!, insertInto: context)
-        
+
         // Get web title
         let contentTextString: String = contentText
         // Save web page title and comments to Core Data
@@ -41,25 +42,25 @@ class ShareViewController: SLComposeServiceViewController {
         newBookmark.setValue(false, forKey: "isImportant")
         newBookmark.setValue(false, forKey: "isDone")
 
-        
-        
+
+
         let calendar = Calendar.current
         let startOfToday = calendar.startOfDay(for: Date())
-        
+
         var components = DateComponents()
         components.setValue(1, for: .day)
         let dateScheduled = Calendar.current.date(byAdding: components, to: startOfToday)
 
         newBookmark.setValue(dateScheduled, forKey: "date")
         newBookmark.setValue(selectedGoal, forKey: "goalAssigned")
-        
+
         // Get web URL
         if let item = extensionContext?.inputItems[0] as? NSExtensionItem {
-            
+
             if let itemProviders = item.attachments {
-                
+
                 for itemProvider: NSItemProvider in itemProviders {
-                
+
                     // info.plist
                     // <key>NSExtensionActivationSupportsWebURLWithMacCount</key>
                     // <integer>1</integer>
@@ -68,11 +69,11 @@ class ShareViewController: SLComposeServiceViewController {
                             if let shareURL = url as? URL {
                                 // Save url to Core Data
                                 newBookmark.setValue(shareURL, forKey: "url")
-                                
+
                                 //newBookmark.url = shareURL
-                                
+
                                 self.saveContext()
-                                
+
                                 print(" ")
                                 print("if let shareURL = url as? URL was true")
                                 print("shareURL: \(shareURL)")
@@ -82,7 +83,7 @@ class ShareViewController: SLComposeServiceViewController {
 //
 //                            itemProvider.loadItem(forTypeIdentifier: "public.plain-text", options: nil, completionHandler: { (string, error) -> Void in
 //                                if let string = (string as? String), let shareURL = URL(string: string) {
-//                                    
+//
 //                                    newBookmark.setValue(shareURL, forKey: "url")
 //                                    self.saveContext()
 //                                    print(" ")
@@ -90,14 +91,14 @@ class ShareViewController: SLComposeServiceViewController {
 //                                    print("shareURL: \(shareURL)")
 //                                }
 //                            })
-                        
-                        
-                        
+
+
+
                     } else if itemProvider.hasItemConformingToTypeIdentifier(kUTTypePlainText as String) {
-                        
+
                         itemProvider.loadItem(forTypeIdentifier: kUTTypePlainText as String, options: nil, completionHandler: { (string, error) -> Void in
                             if let string = (string as? String), let shareURL = URL(string: string) {
-                                
+
                                 newBookmark.setValue(shareURL, forKey: "url")
                                 self.saveContext()
                                 print(" ")
@@ -105,9 +106,9 @@ class ShareViewController: SLComposeServiceViewController {
                                 print("shareURL: \(shareURL)")
                             }
                         })
-                    
+
                     }
-                    
+
                     // Grab preview
                               //                        itemProvider.loadPreviewImage(options: nil, completionHandler: { (item, error) in
                               //                            if let image = item as? UIImage {
@@ -128,8 +129,8 @@ class ShareViewController: SLComposeServiceViewController {
 //    override var preferredContentSize: CGSize {
 //         get {
 //             if let fullSize = self.presentingViewController?.view.bounds.size {
-//                 return CGSize(width: fullSize.width * 0.5,
-//                               height: fullSize.height * 0.75)
+//                return CGSize(width: fullSize.width * 1.0,
+//                              height: fullSize.height * 1.0)
 //             }
 //             return super.preferredContentSize
 //         }
@@ -140,9 +141,33 @@ class ShareViewController: SLComposeServiceViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Catalyst doesn't properly support iOS Share Extension SLComposeServiceViewController default view
+        // The bottom of the view is cut.
+        // For now, disable for MacOS version
+        // Possible to create a customized UIViewController instead of SLComposeServiceViewController
+        // But need re-constrution of this whole code
+//        #if targetEnvironment(macCatalyst)
+//        self.preferredContentSize = NSSize(width: 200, height: 1000)
+////        self.view.frame.size.height = 1000
+////        self.view.bounds.size.height = 1000
+//        print("")
+//        print("#if touched")
+//
+//        #else
+//
+//        #endif
+
         
-//        self.preferredContentSize = CGSize(width: 100, height: 500)
+//        if #available(iOS 13.0, *) {
+//            _ = NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidShowNotification, object: nil, queue: .main) { (_) in
+//                if let layoutContainerView = self.view.subviews.last {
+//                    layoutContainerView.frame.size.height += 50
+//                }
+//            }
+//        }
         
+//        self.preferredContentSize = CGSize(width: 200, height: 500)
         
         placeholder = NSLocalizedString("Type something here to activate 'Post' button.", comment: "Placeholder")
         
